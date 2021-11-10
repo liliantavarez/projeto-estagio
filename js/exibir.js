f8ed0c6edaaef88b970c56b64e378ad1495c8bdc
Lilian Carvalho <liliancarvalhotavares@gmail.com>
2021-11-10 11:02:59 -0300

    refatoração na função de exibir

---
+++ js/exibir.js
@@ -0,0 +1,15 @@
+let cadastrados = JSON.parse(localStorage.getItem("arrayCadastros"));
+
+function exibirNaTela(cadastrados) {
+  let html = "";
+  for (var i = 0; i < cadastrados.length; i++) {
+    html += "<tr><td>" + cadastrados[i].numId + "</td>";
+    html += "<td>" + cadastrados[i].especie + "</td>";
+    html += "<td>" + cadastrados[i].porte + "</td>";
+    html += "<td >" + cadastrados[i].descricao + "</td>";
+  }
+  let tabela = document.getElementById("exibirCadastros");
+  tabela.innerHTML = html;
+}
+
+exibirNaTela(cadastrados);
--- js/script.js
+++ js/script.js
@@ -50,18 +50,4 @@ class Cadastro {
 }
 
 let fazerCadastros = new Cadastro();
-let cadastrados = JSON.parse(localStorage.getItem("arrayCadastros"));
 
-function exibirNaTela(cadastrados) {
-  var html = "";
-  for (var i = 0; i < cadastrados.length; i++) {
-    html += "<tr><td>" + cadastrados[i].numId + "</td>";
-    html += "<td>" + cadastrados[i].especie + "</td>";
-    html += "<td>" + cadastrados[i].porte + "</td>";
-    html += "<td >" + cadastrados[i].descricao + "</td>";
-  }
-  var tabela = document.getElementById("exibirCadastros");
-  tabela.innerHTML = html;
-}
-
-exibirNaTela(cadastrados);
